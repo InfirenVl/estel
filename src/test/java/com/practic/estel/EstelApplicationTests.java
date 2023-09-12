c084414ed968afd5a06cdd146dff6c52b3d06e2e
infiren <vlad.s.bykov@gmail.com>
2023-09-12 21:08:12 +0500

    initial commit

---
+++ src/test/java/com/practic/estel/EstelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practic.estel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
