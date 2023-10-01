a866ff669dbba3d566ef69d7c38bcee3c67a6043
Cr1spyL0ad <bunkermaika2@gmail.com>
2023-10-02 04:44:05 +0500

    Сделал нормальную структуру проекта, закончил регистрацию примерно на 60 процентов

---
+++ src/main/java/com.estel/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.estel.repository;
+
+import com.estel.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com.estel/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.estel.repository;
+
+import com.estel.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
